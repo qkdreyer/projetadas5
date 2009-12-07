725fccf82c278fa5236a0bda53945be72e5f9411
pierre.jambet <pierre.jambet@3406cf02-e2b3-11de-9741-e90c74c3d541>
2009-12-07 21:06:41 +0000

    Mise a jour des specifs fonctionelles sur les packages mots,listegen et text_stat

--- trunk/src/mot.adb
+++ trunk/src/mot.adb
@@ -1,3 +1,38 @@
 package body mot is
+   
+   function Estmot(S: String)return Boolean is
+   begin
+      
+   end;
+   
+   function EstMotSignificatif(T: T_Mot)return boolean is
+   begin
+      return EstLongMot(T) and then EstPetitMot(T) ;    
+   end;
+   
+   function Estlongmot(T: T_Mot) return Boolean is
+   begin
+      return T'length > 3;
+   end;
+   
+   function Estpetitmotimp(T: T_Mot) return Boolean is
+   begin
+      
+   end;
 
+   function Estsuffixede(T1: T_Mot;T2: T_Mot)return Boolean is
+   begin
+      
+   end;
+   
+   function Estprefixede(T1: T_Mot;T2: T_Mot)return Boolean is
+   begin
+      
+   end;
+    
+   function Estfacteurde(T1: T_Mot;T2: T_Mot)return Boolean is
+   begin
+      
+   end;
+   
 end mot;
--- trunk/src/text_stat.ads
+++ trunk/src/text_stat.ads
@@ -1,18 +1,23 @@
+with Liste_Couple;use Liste_Couple
 package Text_Stat is
    --Ce paquetage permet d'effectuer des opérations "statistiques" sur le texte analysé
    --nombre total de mots, nombre de mots de longueur supérieur ou égale a un entier positif donné,
    --longueur moyenne d'un mot, et surement d'autres plus tard
 
-   function Num_Mot_Tot(Texte) return Integer;
+   function Num_Mot_Tot(txt: Fichier_Texte) return Integer;
    --Calcule le nombre total de mot du texte
    
-   function Num_Occ_Moy(?) return Integer;
-   --Calcule le nombre d'occurence moyen... Je sais pas ce que c'est ...
+   function Num_Occ_Moy((L: ListeCouple) return Float;
+   --Calcule le nombre d'occurence moyen a partir de la liste obtenu des mots
+   --dans le texte
    
-   function Long_Moy(Texte) return Integer;
+   function Long_Moy(txt: Fichier_Texte) return Integer;
    --Calcule la longueur moyenne d'un mot dans le texte
    
-   function Num_Mot_Sup(Texte;N: Integer) return Integer;
+   function Num_Mot_Sup((L: ListeCouple;N: Integer) return Integer;
    --Calcule le nombre de mot dont la longueur est supérieur ou égale a un entier positif donné
+
+   -- ... A completer eventuellement par la suite
+
 
 end Text_Stat;
