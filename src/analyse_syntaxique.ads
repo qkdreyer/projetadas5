7e74d2ccad35294865a957d60212077f87b8d964
quentin.dreyer <quentin.dreyer@3406cf02-e2b3-11de-9741-e90c74c3d541>
2009-12-07 22:26:19 +0000

    Maj de analyse_syntaxique

--- src/analyse_syntaxique.ads
+++ src/analyse_syntaxique.ads
@@ -3,10 +3,7 @@ package Analyse_Syntaxique is
    --Surement le package principal qui permettra d'analyser le texte et
    --d'effectuer les requetes dessus
    
-   procedure lecture_texte(texte);
-   --Procedure qui permet de lire un texte et de filtrer les mots significatifs
-
-   function query_liste_couple(texte) return liste;
+	function query_liste_couple(texte) return liste;
    --Renvoit une liste comprenant tous les couples(mot;occurence) du texte, la
    --liste est triée par ordre alphabetique
 
@@ -17,29 +14,16 @@ package Analyse_Syntaxique is
    procedure fusion(l: liste;m1: T_Mot;m2: T_Mot);
    --fusionne le mot m2 dans m1, m2 est supprimé de la liste
    
-   function query_occ_mot(m: T_Mot) return Integer;
+   function query_NbOcc(l: liste; m: T_Mot) return Integer;
    --Requete renvoyant le nombre d'occurence du mot m dans le texte
-
-   function query_numb_pref(s: T_Mot) return Integer;
-   --Requete renvoyant le nombre d'occurence de mot ayant s pour prefixe
    
-   --En fait ca j'ai pas bien compris ...
-   function query_occ_pref(s: T_Mot) return Integer;
-   --Requete renvoyant le nombre d'occurence du mot s dans le texte
-    
-   function query_numb_suff(s: T_Mot) return Integer;
-   --Requete renvoyant le nombre d'occurence de mot ayant s pour suffixe
-
-   --En fait ca j'ai pas bien compris ...
-   function query_occ_suff(s: T_Mot) return Integer;
-   --...
-   
-   function query_num_fact(s: T_Mot) return Integer;
-   --Requete renvoyant le nombre d'occurence de mot ayant s pour facteur
-
-   --En fait ca j'ai pas bien compris ...
-   function query_occ_fact(s: T_Mot) return Integer;
-   --...
-
-
-end Analyse_Sy  ntaxique;
+   procedure query_NbPref(l: in liste; s: in T_Mot; nbMot: out Integer; nbOcc: out Integer);
+   --Requete renvoyant le nombre de mot ayant s pour préfixe et le nombre d'occurence de s (Appel de query_NbOcc(s))
+	
+	procedure query_NbSuff(l: in liste; s: in T_Mot; nbMot: out Integer; nbOcc: out Integer);
+	--Requete renvoyant le nombre de mot ayant s pour suffixe et le nombre d'occurence de s	(Appel de query_NbOcc(s))
+	
+	procedure query_NbFact(l: in liste; s: in T_Mot; nbMot: out Integer; nbOcc: out Integer);
+	--Requete renvoyant le nombre de mot ayant s pour facteur et le nombre d'occurence de s (Appel de query_NbOcc(s))
+	
+end Analyse_Syntaxique;
