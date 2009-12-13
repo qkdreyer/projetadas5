c787645a0c9dfdbdfc74f1f95d0d6a958762bb20
pierre.jambet <pierre.jambet@3406cf02-e2b3-11de-9741-e90c74c3d541>
2009-12-13 16:38:42 +0000

    rajout de 2 fonctions dans analyse_syntaxique.ads

--- trunk/src/analyse_syntaxique.ads
+++ trunk/src/analyse_syntaxique.ads
@@ -18,6 +18,21 @@ package Analyse_Syntaxique is
 
 	procedure query_NbFact(l: in liste; s: in T_Mot; nbMot: out Integer; nbOcc: out Integer);
 	--Requete renvoyant le nombre de mot ayant s pour facteur et le nombre d'occurence de s (Appel de query_NbOcc(s))
-
+      
+      procedure Creer_Fichier_Listemot(L: in Liste);
+      --Procedure qui crée le fichier listeMot a partir de la liste de couple l
+      --Le Fichier créé est de la forme suivante
+      ------------------------------------------
+      --Mot1 Nbocc1
+      --Mot2 NbOcc2
+      --etc ...
+      ------------------------------------------
+      
+      procedure Recup_Liste(L: out Liste);
+      --procedure qui recrée la liste de couple a partir du fichier "liste-mot.txt" 
+      --le fichier n'est pas passé en parametre car il sera créé auparavant par la fonction Creer_Fichier_Listemot
+      --et donc le fichier existera forcement et pourra etre ouvert dans le corps de la fonction
+      --declenche une exception si le fichier n'existe pas
+   
 end Analyse_Syntaxique;
 
