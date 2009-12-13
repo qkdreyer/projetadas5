291befe623366018898bbb64ed937ea367018163
quentin.dreyer <quentin.dreyer@3406cf02-e2b3-11de-9741-e90c74c3d541>
2009-12-13 02:03:52 +0000

    ajout d'un main.adb modif de anal_synt / listegen

--- src/analyse_syntaxique.ads
+++ src/analyse_syntaxique.ads
@@ -1,29 +1,23 @@
-with Text_Stat;use Text_Stat;
+with Text_Stat; use Text_Stat;
+
 package Analyse_Syntaxique is
-   --Surement le package principal qui permettra d'analyser le texte et
-   --d'effectuer les requetes dessus
-   
-	function query_liste_couple(texte) return liste;
-   --Renvoit une liste comprenant tous les couples(mot;occurence) du texte, la
-   --liste est triée par ordre alphabetique
+	--Package permettant d'analyser le texte et d'effectuer les requetes dessus
 
-   procedure affiche_liste(l: liste);
-   --affiche les n premiers elements de la liste, n est saisie au clavier par
-   --l'utilisateur
+	function query_liste_couple return liste;
+	--Renvoit une liste comprenant tous les couples(mot;occurence) du texte, la
+	--liste est triée par ordre alphabetique
 
-   procedure fusion(l: liste;m1: T_Mot;m2: T_Mot);
-   --fusionne le mot m2 dans m1, m2 est supprimé de la liste
-   
-   function query_NbOcc(l: liste; m: T_Mot) return Integer;
-   --Requete renvoyant le nombre d'occurence du mot m dans le texte
-   
-   procedure query_NbPref(l: in liste; s: in T_Mot; nbMot: out Integer; nbOcc: out Integer);
-   --Requete renvoyant le nombre de mot ayant s pour préfixe et le nombre d'occurence de s (Appel de query_NbOcc(s))
-	
+	function query_NbOcc(l: liste; m: T_Mot) return Integer;
+	--Requete renvoyant le nombre d'occurence du mot m dans le texte
+
+	procedure query_NbPref(l: in liste; s: in T_Mot; nbMot: out Integer; nbOcc: out Integer);
+	--Requete renvoyant le nombre de mot ayant s pour préfixe et le nombre d'occurence de s (Appel de query_NbOcc(s))
+
 	procedure query_NbSuff(l: in liste; s: in T_Mot; nbMot: out Integer; nbOcc: out Integer);
 	--Requete renvoyant le nombre de mot ayant s pour suffixe et le nombre d'occurence de s	(Appel de query_NbOcc(s))
-	
+
 	procedure query_NbFact(l: in liste; s: in T_Mot; nbMot: out Integer; nbOcc: out Integer);
 	--Requete renvoyant le nombre de mot ayant s pour facteur et le nombre d'occurence de s (Appel de query_NbOcc(s))
-	
+
 end Analyse_Syntaxique;
+
--- src/listegen.adb
+++ src/listegen.adb
@@ -1,7 +1,5 @@
 package body ListeGen is
-   
-   
-   
+
    procedure Liberer is new Ada.Unchecked_Deallocation(Cellule,T_Liste);
    --crée une procedure de liberation d'espace pour les objets
    --désignés par une liste
@@ -29,19 +27,6 @@ package body ListeGen is
       null;
    end Affiche;
 
-   procedure AjoutDebut(L: in out T_Liste;X: in T_Elem) is
-      --
-      Ltemp: T_Liste;
-   begin
-      if EstVide(L) then
-         L:= new Cellule'(X,null,null);
-      else-- !ListeVide(L) then
-         Ltemp:= new Cellule'(X,null,L);
-         L.all.Prec:=Ltemp;
-         L:=Ltemp;
-      end if;
-   end AjoutDebut;
-
    procedure AjoutFin(L: in out T_Liste;X: in T_Elem) is
       ltemp: T_Liste;
    begin
@@ -65,68 +50,6 @@ package body ListeGen is
          return L.all.Val;
       end if;
    end Premier;
-
-   function Dernier(L: T_Liste) return T_Elem is
-      --Version iterative
-      Ltemp: T_Liste;
-   begin
-      Ltemp := L;
-      if EstVide(l) then raise ListeVideException;
-      else
-         while Ltemp.all.Suiv/=null loop
-            Ltemp:=Ltemp.all.Suiv;
-         end loop;
-         return Ltemp.all.Val;
-      end if;
-   end Dernier;
-
-   function NbOcc(L: T_Liste;X: T_Elem) return Integer is
-      --Version recursive
-   begin
-      if EstVide(L) then
-         return 0;
-      elsif L.all.Val=X then
-         return 1+NbOcc(L.all.Suiv,X);
-      else
-         return NbOcc(L.all.Suiv,X);
-      end if;
-   end NbOcc;
-
-   procedure InsererAvant(L: in out T_Liste;X: in T_Elem;Y: T_Elem)is
-      P: T_Liste;
-      ltemp: T_Liste;
-   begin
-      P := L;
-      while P /= null and then P.all.Val /= y loop
-         P := P.all.suiv;
-      end loop;--P = vide ou sinon P.val = y
-      if EstVide(P) then
-         AjoutDebut(L,X);
-      elsif P = L then--x en tete de liste
-         AjoutDebut(L,x);
-      else
-         ltemp := new Cellule'(x,l.all.Prec,P);
-         P.all.Prec.all.Suiv := ltemp;
-         P.all.Prec := ltemp;
-      end if;
-
-   end InsererAvant;
-
-   procedure InsererApres(L: in out T_Liste;X: in T_Elem;Y: T_Elem)is
-      --
-      ltemp: T_Liste;
-   begin
-      ltemp := L;
-      if EstVide(L) then L := new Cellule'(x,null,null);
-      elsif EstVide(l.all.Suiv) then AjoutFin(L,x);
-      elsif L.all.val = y then
-         ltemp := new Cellule'(x,l,l.all.suiv);
-         l.all.suiv.all.prec := ltemp;
-         l.all.suiv := ltemp;
-      else
-         InsererApres(L.all.Suiv,x,y);
-      end if;
-   end InsererApres;
 
    procedure Supprimer(L: in out T_Liste;X: in T_Elem)is
       --
@@ -155,114 +78,28 @@ package body ListeGen is
       end if;
    end Supprimer;
 
-   procedure SupprimerTout(L: in out T_Liste;X: in T_Elem)is
-      --A REFAIRE
-      --A AMELIORER AVEC LE TRUC DE QUENTIN
-      Ltemp: T_Liste;
+   procedure Tri_Gen(L: in out T_Liste) is
+   --Tri alphab�tiquement la liste
    begin
-      Ltemp := L;
-      while Ltemp /= null loop
-         if Premier(L)=X then Supprimer(L,X);
-         end if;
-         Ltemp := Ltemp.all.Suiv;
-      end loop;
-   end SupprimerTout;
 
-   function Index(L: T_Liste;X: T_Elem) return Integer is
-      --
-   begin
-      if EstVide(L) then raise ListeVideException;
-      elsif Premier(L) = x then return 0;
-      else return 1 + Index(l.all.Suiv,x);
-      end if;
-   end Index;
-
-   function EltInd(L: T_Liste;X: Integer) return T_Elem is
-      --x > 0
-   begin
-      if EstVide(l) then raise ListeVideException;
-      elsif x = 0 then return Premier(l);
-      else return EltInd(l.all.Suiv,x-1);
-      end if;
-   end EltInd;
-
-   procedure RetireInd(L: in out T_Liste;X: in Integer)is
-      --
-   begin
-      Supprimer(L,EltInd(L,X));
-   end RetireInd;
-
-   function EtendListe(L1: T_Liste;L2:  T_Liste)return T_Liste Is
-      --
-      P1: T_Liste;
-      P2: T_Liste;
-   begin
-      P1 := L1;P2 := L2;
-      if EstVide(P1) then return P2;
-      elsif EstVide(P2) then return P1;
-      else
-         while P1.Suiv /= null loop
-            P1 := P1.all.Suiv;
-         end loop;--P1 est le dernier element de L1
-         while P2 /= null loop
-            AjoutFin(P1,Premier(P2));--appel en temps constant O(1) car P1 est le dernier element de L1
-            P1 := P1.all.Suiv;--On maintient P1 comme dernier element
-         end loop;
-         return L1;
-      end if;
-   end EtendListe;
-
-   function CopieTriee(L : T_Liste) return T_Liste is
-   begin
-      --TODO
-      return L;
-   end CopieTriee;
-   
-   procedure InsererTriee(L: in out T_Liste;X : in T_Elem) is
-   begin
-      null;
    end;
-            
-   procedure CopieTrieeModif_It(L: in out T_Liste)is
-      --Version Iterative
-      Ltemp: T_Liste;
-      Lcourant: T_Liste;
-      Val_Temp: T_Elem;
-   begin
-      if not(EstVide(L.all.Suiv)) then
-         Ltemp := L.all.Suiv;
-         Lcourant := L.all.Suiv;
-         while Ltemp /= null loop--Parcour de la liste entiere
-            --On place la cellule pointée par Lcourant
-            while (LCourant.all.Prec /= null) and then (Lcourant.all.Val > LCourant.all.Prec.all.Val) loop
-              --On echange lcourant avec son predecesseur si il n'est pas le dernier
-               Val_Temp := LCourant.val;
-               LCourant.all.Val := LCourant.all.Prec.all.Val;
-               LCourant.all.Prec.all.Val := Val_Temp;
-               LCourant := LCourant.all.Prec;
-            end loop;
-            Ltemp := Ltemp.Suiv;
-         end loop;
-      end if;
-   end CopieTrieeModif_It;
 
-   procedure CopieTrieeModif_Rec(L: in out T_Liste) is
-      --Version Recursive
+   procedure Fusion(L: in out T_Liste; m1,m2: T_Mot) is
+      I : Integer;
+      Egal : Boolean;
    begin
-      null;
-   end CopieTrieeModif_Rec;
-
-   function CopieInverse(L: T_Liste)return T_Liste is
-      --
-      ltemp: T_Liste;
-      Linvers: T_Liste;
-   begin
-      Linvers := CreerListe;
-      Ltemp := L;
-      while ltemp /= null loop
-         AjoutDebut(linvers,Premier(ltemp));
+      I := 0;
+      Egal := False;
+      while I < String'Min(m1,m2) and Egal loop
+         if m1(I) /= m2(I) then
+            Egal := False;
+         end if;
+         I := I+1;
       end loop;
-      return linvers;
-   end CopieInverse;
+      if Egal then
+         Supprimer(l, m2);
+      end if;
+   end;
+
 
 end ListeGen;
