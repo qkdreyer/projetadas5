a9fac08f50781d0339bed6f4eee86220776d1707
quentin.dreyer <quentin.dreyer@3406cf02-e2b3-11de-9741-e90c74c3d541>
2009-12-20 18:14:05 +0000

    Fix

--- src/analyse_lexicale.ads
+++ src/analyse_lexicale.ads
@@ -1,19 +1,21 @@
-with Mot;use Mot;
-WITH Liste_Couple;USE Liste_Couple;
+with Mot;
+use Mot;
+with Liste_Couple;
+use Liste_Couple;
 
-PACKAGE Analyse_Lexicale IS
+package Analyse_Lexicale is
    --Package permettant d'analyser le texte et d'effectuer les requetes dessus
 
    --utilisation de la liste generique
-   USE Liste_Couple.L;
+   use Liste_Couple.L;
 
-   FUNCTION CaractereAutorise (
-         C : IN     Character)
-     RETURN Boolean;
+   function CaractereAutorise (
+         C : in     Character)
+     return Boolean;
 
-   PROCEDURE Query_Liste_Couple (
-         L      : IN OUT TListe_Couple;
-         NomFic : IN     String);
+   procedure Query_Liste_Couple (
+         L      : in out TListe_Couple;
+         NomFic : in     String);
    --Renvoit une liste comprenant tous les couples(mot;occurence) du texte, la
    --liste est triée par ordre alphabetique
    --
@@ -21,35 +23,35 @@ PACKAGE Analyse_Lexicale IS
    --et qu'a chaque mot trouvé on l'insere de maniere triéee dans la liste qu'on renvoit
    --si on le trouve dans la liste alors on fait juste augmenter le nbOcc
 
-   FUNCTION Query_NbOcc (
+   function Query_NbOcc (
          L : TListe_Couple;
          M : String)
-     RETURN Integer;
+     return Integer;
    --Requete renvoyant le nombre d'occurence du mot m dans le texte
 
-   PROCEDURE Query_NbPref (
-         L     : IN     TListe_Couple;
-         S     : IN     String;
-         NbMot :    OUT Integer;
-         NbOcc :    OUT Integer);
+   procedure Query_NbPref (
+         L     : in     TListe_Couple;
+         S     : in     String;
+         NbMot :    out Integer;
+         NbOcc :    out Integer);
    --Requete renvoyant le nombre de mot ayant s pour préfixe et le nombre d'occurence de s (Appel de query_NbOcc(s))
 
-   PROCEDURE Query_NbSuff (
-         L     : IN     TListe_Couple;
-         S     : IN     String;
-         NbMot :    OUT Integer;
-         NbOcc :    OUT Integer);
+   procedure Query_NbSuff (
+         L     : in     TListe_Couple;
+         S     : in     String;
+         NbMot :    out Integer;
+         NbOcc :    out Integer);
    --Requete renvoyant le nombre de mot ayant s pour suffixe et le nombre d'occurence de s      (Appel de query_NbOcc(s))
 
-   PROCEDURE Query_NbFact (
-         L     : IN     TListe_Couple;
-         S     : IN     String;
-         NbMot :    OUT Integer;
-         NbOcc :    OUT Integer);
+   procedure Query_NbFact (
+         L     : in     TListe_Couple;
+         S     : in     String;
+         NbMot :    out Integer;
+         NbOcc :    out Integer);
    --Requete renvoyant le nombre de mot ayant s pour facteur et le nombre d'occurence de s (Appel de query_NbOcc(s))
 
-   PROCEDURE Creer_Fichier_Listemot (
-         L : IN     TListe_Couple);
+   procedure Creer_Fichier_Listemot (
+         L : in     TListe_Couple);
    --Procedure qui crée le fichier "liste-mot.txt" a partir de la liste de couple l
    --Le Fichier créé est de la forme suivante
    ------------------------------------------
@@ -58,22 +60,25 @@ PACKAGE Analyse_Lexicale IS
    --etc ...
    ------------------------------------------
 
-   FUNCTION Existe (
-         Name : IN     String)
-     RETURN Boolean;
+   function Existe (
+         Name : in     String)
+     return Boolean;
 
-   PROCEDURE Recup_Liste (
-         L : IN OUT TListe_Couple);
+   procedure Recup_Liste (
+         L : in out TListe_Couple);
    --procedure qui recrée la liste de couple a partir du fichier "liste-mot.txt"
    --le fichier n'est pas passé en parametre car il sera créé auparavant par la fonction Creer_Fichier_Listemot
    --et donc le fichier existera forcement et pourra etre ouvert dans le corps de la fonction
    --declenche une exception si le fichier n'existe pas
-   
-procedure AffichageN (
+
+   procedure AffichageN (
          L : in     TListe_Couple;
          N : in     Integer);
-      -- affiche les N premiers mots de la liste
+   -- affiche les N premiers mots de la liste
+   
+   procedure AffichageListe (
+         L : in     TListe_Couple);
+      -- affiche les elements de la liste
 
-
-END Analyse_Lexicale;
+   end Analyse_Lexicale;
 
--- src/main.adb
+++ src/main.adb
@@ -2,6 +2,7 @@ with Analyse_Lexicale;
 use Analyse_Lexicale;
 with Liste_Couple;
 use Liste_Couple;
+with Listegen;
 with Text_Stat;
 use Text_Stat;
 with Ada.Text_IO;
@@ -11,13 +12,14 @@ use Ada.Integer_Text_IO;
 
 procedure Main is
 
-   Menu   : Boolean;
-   Choix  : Integer;
-   L      : TListe_Couple;
-   Buffer : String (1 .. 30);
-   Last   : Natural;
-   NomFic : Boolean;
-   NbMot  : Integer;
+   Menu      : Boolean;
+   Choix     : Integer;
+   L         : TListe_Couple;
+   M         : TListe_Couple;
+   Buffer    : String (1 .. 30);
+   Last      : Natural;
+   NomFic    : Boolean;
+   NbMot     : Integer;
 
 begin
 
@@ -27,6 +29,7 @@ begin
       Put_Line("Entrez 2 pour enregistrer la liste dans le fichier " & Character'Val(34) & "liste-mots.txt" & Character'Val(34) & ".");
       Put_Line("Entrez 3 pour recuperer la liste a partir du fichier " & Character'Val(34) & "liste-mots.txt" & Character'Val(34) & ".");
       Put_Line("Entrez 4 pour afficher les mots les plus utilises.");
+      Put_Line("Entrez 5 pour afficher la liste.");
       Put_Line("Entrez 0 pour quitter.");
       New_Line;
       Get(Choix);
@@ -69,7 +72,11 @@ begin
             Get(NbMot);
             New_Line;
             --Tri L par NBOCC
-            AffichageN(L, NbMot);
+            AffichageN(M, NbMot);
+
+         when 5 =>
+
+            AffichageListe(L);
 
          when others =>
 
