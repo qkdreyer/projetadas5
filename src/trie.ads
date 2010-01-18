71bd2e7c95bec4cfaf6f26abc6bededbdf041fde
quentin.dreyer <quentin.dreyer@3406cf02-e2b3-11de-9741-e90c74c3d541>
2010-01-18 20:29:18 +0000

    working trie !

--- src/trie.ads
+++ src/trie.ads
@@ -2,27 +2,37 @@ with Mot;
 use Mot;
 
 package Trie is
-      
-      type T_Trie is private;
-         
-      function CreerTrie return T_Trie;      
-      function TrieVide(T : T_Trie) return Boolean;
-      function AjouterMot(T : T_Trie; M : T_Mot) return T_Trie;
-      -- si T = Vide alors crée un T contenant E
-      function CompteMots(T : T_Trie; M : T_Mot) return Integer;
-      -- si T = Vide alors retourne 0
-      function ComptePrefixe(T : T_Trie; M : T_Mot) return Integer;
-      -- si T = Vide alors retourne 0
 
-      private
-         
-      type Tab is array (Character) of T_Trie;
-      type Celulle;
-      type T_Trie is access Celulle;
-      type Celulle is record
-         ST : Tab;
-         Mots : Integer;
-         Prefixes : Integer;
-      end record;
-      
+   type T_Trie is private;
+
+   function CreerTrie return T_Trie;
+   function TrieVide(T : in T_Trie) return Boolean;
+
+   function AjouterMot(T : in T_Trie; M : in T_Mot) return T_Trie;
+   -- si T = Vide alors crée un T contenant E
+
+   function CompteMots(T : in T_Trie; M : in T_Mot) return Integer;
+   -- si T = Vide alors retourne 0
+
+   function ComptePrefixe(T : in T_Trie; M : in T_Mot) return Integer;
+   -- si T = Vide alors retourne 0
+
+   procedure AfficheTrie(T : in T_Trie);
+
+   procedure AffichageN(T : in T_Trie; N : in Integer);
+   -- affiche les N premiers mots du trie
+
+private
+
+   subtype Tindice is character range 'a' .. 'z';
+
+   type Tab is array (Tindice) of T_Trie;
+   type Celulle;
+   type T_Trie is access Celulle;
+   type Celulle is record
+      ST : Tab;
+      Mots : Integer;
+      Prefixes : Integer;
+   end record;
+
 end Trie;
